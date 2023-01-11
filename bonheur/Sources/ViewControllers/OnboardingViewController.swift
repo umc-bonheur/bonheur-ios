9b1a1f9b1102455c96456ff0539e1055f6954720
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-01-11 13:35:17 +0900

    [FEAT] 최초 실행 화면 UI Component 구성

--- bonheur/Sources/ViewControllers/OnboardingViewController.swift
+++ bonheur/Sources/ViewControllers/OnboardingViewController.swift
@@ -8,9 +8,8 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
-    let mainLabel = UILabel()
     
+    // back button
     lazy var backButton: UIButton = {
         let button = UIButton(type: .custom)
         button.setTitle("Back", for: .normal)
@@ -21,7 +20,112 @@ class OnboardingViewController: UIViewController {
         return button
     }()
     
-    var someString: String?
+    // title
+    private var subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "나만의 작은 행복 보관함"
+        label.font = UIFont(name: "NanumSquareRoundOTFEB", size: 16)
+        
+        return label
+    }()
+    
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "bonheur"
+        label.font = UIFont(name: "NanumSquareRoundOTFEB", size: 48)
+        
+        return label
+    }()
+    
+    private lazy var titleStackView: UIStackView = {
+        let st = UIStackView(arrangedSubviews: [subTitleLabel, titleLabel])
+        st.axis = .vertical
+        st.distribution = .fill
+        st.alignment = .center
+        
+        return st
+    }()
+    
+    // logo
+    private var logoImageView = UIImageView(image: UIImage(named: "AppLogo"))
+    
+    // oauth
+    private lazy var kakaoLoginButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        var title = "카카오톡으로 시작하기"
+        config.title = title
+        config.attributedTitle = AttributedString(title, attributes: AttributeContainer([NSAttributedString.Key.font: UIFont(name: "SFPro-Bold", size: 16)!]))
+        config.image = UIImage(named: "KakaoTalkLogo")
+        config.imagePlacement = .leading
+        config.imagePadding = 12
+        config.baseBackgroundColor = UIColor(red: 0.996, green: 0.898, blue: 0, alpha: 1)
+        config.baseForegroundColor = UIColor(red: 0.149, green: 0.15, blue: 0.149, alpha: 1)
+        
+        config.background.cornerRadius = 13
+        config.cornerStyle = .fixed
+        
+        return UIButton(configuration: config)
+    }()
+    
+    private lazy var appleLoginButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        var title = "Apple로 시작하기"
+        config.title = title
+        config.attributedTitle = AttributedString(title, attributes: AttributeContainer([NSAttributedString.Key.font: UIFont(name: "SFPro-Bold", size: 16)!]))
+        config.image = UIImage(named: "AppleLogo")
+        config.imagePlacement = .leading
+        config.imagePadding = 8
+        config.baseBackgroundColor = .white
+        config.baseForegroundColor = .black
+        
+        config.background.cornerRadius = 13
+        config.cornerStyle = .fixed
+        
+        return UIButton(configuration: config)
+    }()
+    
+    lazy var buttonStackView: UIStackView = {
+        let st = UIStackView(arrangedSubviews: [kakaoLoginButton, appleLoginButton])
+        st.spacing = 12
+        st.axis = .vertical
+        st.distribution = .fillEqually
+        st.alignment = .fill
+
+        return st
+    }()
+    
+    // email
+    private var emailLoginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이메일로 로그인"
+        label.font = UIFont(name: "SFPro-Regular", size: 12)
+        label.textAlignment = .center
+        label.backgroundColor = .lightGreen
+        
+        return label
+    }()
+    
+    private var emailRegisterLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이메일로 회원가입"
+        label.font = UIFont(name: "SFPro-Regular", size: 12)
+        label.textAlignment = .center
+        label.backgroundColor = .lightGreen
+        
+        return label
+    }()
+
+    lazy var emailStackView: UIStackView = {
+        let st = UIStackView(arrangedSubviews: [emailLoginLabel, emailRegisterLabel])
+        st.spacing = 1
+        st.axis = .horizontal
+        st.distribution = .fillEqually
+        st.alignment = .fill
+        
+        st.backgroundColor = UIColor(red: 0.149, green: 0.15, blue: 0.149, alpha: 1)
+        
+        return st
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,31 +135,67 @@ class OnboardingViewController: UIViewController {
     }
     
     func setup() {
-        mainLabel.text = someString
-        mainLabel.font = UIFont.systemFont(ofSize: 22)
-        
-        view.addSubview(mainLabel)  // 화면에 표시되게 만듦
         view.addSubview(backButton)
+        view.addSubview(titleStackView)
+        view.addSubview(logoImageView)
+        view.addSubview(buttonStackView)
+        view.addSubview(emailStackView)
         
-        view.backgroundColor = .white
+        view.backgroundColor = .lightGreen
     }
-    
-    
+
     func makeAutoLayout() {
-        
-        mainLabel.translatesAutoresizingMaskIntoConstraints = false // 자동적으로 할당된 오토레이아웃 해제
-        mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        mainLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
+        // 뒤로 가기 버튼
         backButton.translatesAutoresizingMaskIntoConstraints = false // 자동적으로 할당된 오토레이아웃 해제
-        backButton.widthAnchor.constraint (equalToConstant: 70).isActive = true
-        backButton.heightAnchor.constraint (equalToConstant: 40).isActive = true
-        backButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
-        backButton.centerXAnchor.constraint (equalTo: view.centerXAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            backButton.widthAnchor.constraint(equalToConstant: 70),
+            backButton.heightAnchor.constraint(equalToConstant: 40),
+            backButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40),
+            backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        // 소셜로그인 버튼 스택 뷰
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            buttonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonStackView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 80),
+            buttonStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            buttonStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+        
+        kakaoLoginButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            kakaoLoginButton.heightAnchor.constraint(equalToConstant: 46)
+        ])
+
+        appleLoginButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            appleLoginButton.heightAnchor.constraint(equalToConstant: 46)
+        ])
+        
+        // 로고 이미지
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -36)
+        ])
+        
+        // 제목 스택 뷰
+        titleStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleStackView.bottomAnchor.constraint(equalTo: logoImageView.topAnchor, constant: -36)
+        ])
+        
+        emailStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            emailStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emailStackView.widthAnchor.constraint(equalTo: buttonStackView.widthAnchor),
+            emailStackView.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor, constant: 32)
+        ])
+        
     }
-    
-    
-    
+        
     @objc func backButtonTapped() {
         dismiss(animated: true, completion: nil) // 전 화면으로 돌아가기
     }
