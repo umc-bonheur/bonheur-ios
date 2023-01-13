cd4d95dbac812067299a5348abf562104cfcc9d3
Betmoong <rdev99@naver.com>
2023-01-14 02:52:05 +0900

    [FIX] HomeView navigation bar 제거

--- bonheur/Sources/ViewControllers/HomeView/HomeViewController.swift
+++ bonheur/Sources/ViewControllers/HomeView/HomeViewController.swift
@@ -14,29 +14,26 @@ class HomeViewController: UIViewController {
     
     
     var userName: String = "짱제이"
-//    lazy var topLabelText: String? = "\(self.userName)님의 행복 기록"
-//
-//    lazy var topLabel: UILabel = {
-//        let label = UILabel()
-//        label.text = self.topLabelText
-//        label.font = UIFont.boldSystemFont(ofSize: 24)
-//        return label
-//    }()
-//
-//    lazy var cloverCalendarButton: UIButton = {
-//        let button = UIButton()
-//        button.setBackgroundImage(UIImage(named: "CloverCalendar"), for: .normal)
-//        button.sizeThatFits(CGSize(width: 24, height: 25.2))
-//        button.tintColor = .black
-//
-//        // 버튼 눌렀을시 cloverCalendarButtonTapped 함수 실행
-//        button.addTarget(self, action: #selector(cloverCalendarButtonTapped), for: .touchUpInside)
-//
-//        return button
-//    }()
-    
-    
-    
+    lazy var topLabelText: String? = "\(self.userName)님의 행복 기록"
+
+    lazy var topLabel: UILabel = {
+        let label = UILabel()
+        label.text = self.topLabelText
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        return label
+    }()
+
+    lazy var cloverCalendarButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named: "CloverCalendar"), for: .normal)
+        button.sizeThatFits(CGSize(width: 24, height: 25.2))
+        button.tintColor = .black
+
+        // 버튼 눌렀을시 cloverCalendarButtonTapped 함수 실행
+        button.addTarget(self, action: #selector(cloverCalendarButtonTapped), for: .touchUpInside)
+
+        return button
+    }()
     
     
     public var homePostingCollectionView: UICollectionView = {
@@ -60,7 +57,7 @@ class HomeViewController: UIViewController {
         homePostingCollectionView.delegate = self
         homePostingCollectionView.dataSource = self
         
-        [homeSortStackView, homePostingCollectionView].forEach {
+        [topLabel, cloverCalendarButton, homeSortStackView, homePostingCollectionView].forEach {
             view.addSubview($0)
         }
         
@@ -73,7 +70,6 @@ class HomeViewController: UIViewController {
         
         setConstraints()
         
-        setNavigation()
     }
     
     
@@ -92,19 +88,19 @@ class HomeViewController: UIViewController {
     
     
     private func setConstraints() {
-//        topLabel.translatesAutoresizingMaskIntoConstraints = false
-//        cloverCalendarButton.translatesAutoresizingMaskIntoConstraints = false
+        topLabel.translatesAutoresizingMaskIntoConstraints = false
+        cloverCalendarButton.translatesAutoresizingMaskIntoConstraints = false
         homeSortStackView.translatesAutoresizingMaskIntoConstraints = false
         homePostingCollectionView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-//            topLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
-//            topLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-//
-//            cloverCalendarButton.topAnchor.constraint(equalTo: topLabel.topAnchor),
-//            cloverCalendarButton.leadingAnchor.constraint(equalTo: topLabel.trailingAnchor, constant: 4),
+            topLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            topLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+
+            cloverCalendarButton.topAnchor.constraint(equalTo: topLabel.topAnchor),
+            cloverCalendarButton.leadingAnchor.constraint(equalTo: topLabel.trailingAnchor, constant: 4),
             
-            homeSortStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 170),
+            homeSortStackView.topAnchor.constraint(equalTo: topLabel.bottomAnchor, constant: 36),
             homeSortStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -19),
             
             homePostingCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 231),
@@ -115,63 +111,6 @@ class HomeViewController: UIViewController {
     }
     
     
-    
-    
-    func setNavigation() {
-        
-        let titleName = UILabel()
-        //titleName.text = "\(self.userName)님의 행복 기록"
-        titleName.font = UIFont(name: "NanumSquareRoundOTFB", size:  24)
-        //titleName.textColor = .magenta
-        //titleName.sizeToFit()
-        
-        
-        self.navigationItem.titleView = titleName
-        
-        //self.navigationItem.title = "\(self.userName)님의 행복 기록"
-        
-        // 네비게이션바 우측에 캘린더 버튼 만들기
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "CloverCalendar"), style: .done, target: self, action: #selector(cloverCalendarButtonTapped))
-        
-        
-        UINavigationBarAppearance().configureWithTransparentBackground()  // 투명으로
-
-        
-        
-        
-       
-        let customView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 310.0, height: 44.0))
-        //customView.backgroundColor = UIColor.yellow
-
-        //var button = UIButton.init(type: .custom)
-        //button.setBackgroundImage(UIImage(named: "hamburger"), for: .normal)
-        //button.frame = CGRect(x: 0.0, y: 5.0, width: 32.0, height: 32.0)
-        //button.addTarget(self, action: #selector(menuOpen(button:)), for: .touchUpInside)
-        //customView.addSubview(button)
-
-        //var marginX = CGFloat(button.frame.origin.x + button.frame.size.width + 5)
-        let label = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: 310.0, height: 140.0))
-        label.text = "\(self.userName)님의 행복 기록"
-        label.font = UIFont(name: "NanumSquareRoundOTFB", size:  24)
-        label.textAlignment = NSTextAlignment.left
-        //label.backgroundColor = UIColor.red
-        customView.addSubview(label)
-        
-        
-        let leftButton = UIBarButtonItem(customView: customView)
-        self.navigationItem.leftBarButtonItem = leftButton
-
-        
-        
-        
-        
-        // 네비게이션 바 뒤로가기 버튼(버튼만 나오게)
-        let backButton = UIBarButtonItem()
-        backButton.title = ""
-        backButton.tintColor = .black
-        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-        
-    }
     
     @objc func cloverCalendarButtonTapped() {
         let calendarVC = CalendarViewController()
