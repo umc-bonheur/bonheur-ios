63bc2871d2a0606079f0c5b5c516b2e106cd07a0
SohyeonKim-dev <happythgus89@gmail.com>
2023-01-25 16:56:27 +0900

    [FEAT] tagAddingTextField 구현 및 extension 추가

--- bonheur/Sources/ViewControllers/TagSelectModalViewController.swift
+++ bonheur/Sources/ViewControllers/TagSelectModalViewController.swift
@@ -28,27 +28,47 @@ class TagSelectModalViewController: UIViewController {
         return button
     }()
     
+    private var tagAddingTextField: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .black
+        textField.placeholder = "해시태그 추가"
+        // textField.textColor =
+        textField.backgroundColor = .clear
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        [guidingTextLabel, modalDismissButton].forEach {
+        [guidingTextLabel, modalDismissButton, tagAddingTextField].forEach {
             view.addSubview($0)
         }
         
+        tagAddingTextField.becomeFirstResponder()
         setConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tagAddingTextField.setUnderLine(lineColor: .lightGray)
     }
     
     private func setConstraints() {
         guidingTextLabel.translatesAutoresizingMaskIntoConstraints = false
         modalDismissButton.translatesAutoresizingMaskIntoConstraints = false
+        tagAddingTextField.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             guidingTextLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 23),
             guidingTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
             modalDismissButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 27.28),
-            modalDismissButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -18.28)
+            modalDismissButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -18.28),
+            
+            tagAddingTextField.topAnchor.constraint(equalTo: guidingTextLabel.bottomAnchor, constant: 31),
+            tagAddingTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tagAddingTextField.widthAnchor.constraint(equalToConstant: 323)
             
         ])
     }
