fd456089d56974748f07d42fa9ed8f0dce186159
Sarang Kim <vvtkfkddl11@naver.com>
2023-02-01 02:09:55 +0900

    [FEAT] Constant, UIImage+Extension, UIViewController+Extension 추가

---
+++ bonheur/Sources/Extensions/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  bonheur
+//
+//  Created by 김사랑 on 2023/01/31.
+//
+
+import Foundation
+import UIKit
+
+struct Constant {
+    static let height = UIScreen.main.bounds.size.height / 813
+    static let width = UIScreen.main.bounds.size.width / 375
+}
