f1281b3685dc8dfd2e90e3b2930acc2acc2f57f8
Sarang Kim <vvtkfkddl11@naver.com>
2023-02-06 21:12:17 +0900

    [FIX] UIViewController+Extension 코드 삭제

--- bonheur/Sources/Extensions/UIViewController+Extension.swift
+++ bonheur/Sources/Extensions/UIViewController+Extension.swift
@@ -9,28 +9,5 @@ import UIKit
 
 extension UIViewController {
     
-    func setupNavigationBackButton(_ sender: UIImage? = nil) {
-        if sender == nil {
-            let backButtonSpacer = UIBarButtonItem()
-            backButtonSpacer.width = -28
-            let backButton = UIBarButtonItem(image: UIImage(named: "chevron.left")?.withRenderingMode(.alwaysOriginal),
-                                             style: .plain,
-                                             target: self,
-                                             action: #selector(didTapBackButton))
-            navigationItem.setLeftBarButtonItems([backButtonSpacer, backButton], animated: false)
-        } else {
-            let senderImageItem = UIBarButtonItem(image: sender?.resize(newWidth: 10.84).withTintColor(.black, renderingMode: .alwaysOriginal),
-                                                  style: .plain,
-                                                  target: self,
-                                                  action: #selector(didTapBackButton))
-
-            navigationItem.setLeftBarButtonItems([senderImageItem], animated: false)
-        }
-    }
     
-    @objc
-    func didTapBackButton() {
-            navigationController?.popViewController(animated: false)
-            tabBarController?.selectedIndex = 0
-    }
 }
