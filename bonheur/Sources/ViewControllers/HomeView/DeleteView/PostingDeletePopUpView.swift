68fae14f873fe3a582ca9f09ee8f74eed45a40a8
SohyeonKim-dev <happythgus89@gmail.com>
2023-02-11 16:38:40 +0900

    [FEAT] DeleteView 구현

--- bonheur/Sources/ViewControllers/HomeView/DeleteView/PostingDeletePopUpView.swift
+++ bonheur/Sources/ViewControllers/HomeView/DeleteView/PostingDeletePopUpView.swift
@@ -7,21 +7,42 @@
 
 import UIKit
 
-// TODO: 게시물 삭제 Alert CustomView
+// 게시물 삭제 Alert CustomView
 class PostingDeletePopUpView: UIView {
     
     let backgroundLabel: UILabel = {
         let label = UILabel()
-        label.frame = CGRect(x: 0, y: 0, width: 257, height: 213)
         label.layer.cornerRadius = 13
-        label.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        label.backgroundColor = .green
+        label.widthAnchor.constraint(equalToConstant: 257).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 213).isActive = true
         return label
+    }()
+    
+    let deleteGuidingText: UITextView = {
+        let deleteGuidingText = UITextView()
+        deleteGuidingText.text = "행복기록을 삭제하겠습니까?"
+        deleteGuidingText.backgroundColor = .clear
+        deleteGuidingText.textColor = UIColor(red: 0.149, green: 0.15, blue: 0.149, alpha: 1)
+        deleteGuidingText.font = UIFont(name: "SFPro-Medium", size: 14)
+        deleteGuidingText.widthAnchor.constraint(equalToConstant: 170).isActive = true
+        return deleteGuidingText
+    }()
+    
+    let warningGuidingText: UITextView = {
+        let warningGuidingText = UITextView()
+        warningGuidingText.text = "삭제된 행복 기록은 되돌릴 수 없습니다"
+        warningGuidingText.backgroundColor = .clear
+        warningGuidingText.textColor = UIColor(red: 0.544, green: 0.55, blue: 0.545, alpha: 1)
+        warningGuidingText.font = UIFont(name: "SFPro-Regular", size: 12)
+        warningGuidingText.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        return warningGuidingText
     }()
     
     init() {
         super.init(frame: .zero)
         
-        [backgroundLabel].forEach {
+        [backgroundLabel, deleteGuidingText, warningGuidingText].forEach {
             addSubview($0)
         }
         
@@ -30,7 +51,20 @@ class PostingDeletePopUpView: UIView {
     
     private func setConstraints() {
         backgroundLabel.translatesAutoresizingMaskIntoConstraints = false
+        backgroundLabel.layer.zPosition = -1
+        deleteGuidingText.translatesAutoresizingMaskIntoConstraints = false
+        warningGuidingText.translatesAutoresizingMaskIntoConstraints = false
         
+        NSLayoutConstraint.activate([
+            backgroundLabel.topAnchor.constraint(equalTo: topAnchor, constant: 328),
+            backgroundLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            deleteGuidingText.topAnchor.constraint(equalTo: backgroundLabel.topAnchor, constant: 26),
+            deleteGuidingText.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            warningGuidingText.topAnchor.constraint(equalTo: backgroundLabel.topAnchor, constant: 63),
+            warningGuidingText.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
     }
     
     required init?(coder: NSCoder) { fatalError() }
--- bonheur/Sources/ViewControllers/HomeView/DeleteView/PostingEditPopUpViewController.swift
+++ bonheur/Sources/ViewControllers/HomeView/DeleteView/PostingEditPopUpViewController.swift
@@ -16,26 +16,35 @@ class PostingEditPopUpViewController: UIViewController {
         view.backgroundColor = .clear
         view.layer.backgroundColor = UIColor(red: 0.149, green: 0.15, blue: 0.149, alpha: 0.4).cgColor
 
-        [postingEditPopUpView, postingDeletePopUpView].forEach {
+        postingEditPopUpView.postingDeleteButton.addTarget(self, action: #selector(self.postingDeleteButtonTapped), for: .touchUpInside)
+        
+        [postingDeletePopUpView].forEach {
             view.addSubview($0)
         }
         
         setConstraints()
     }
-
+    
     private func setConstraints() {
         postingEditPopUpView.translatesAutoresizingMaskIntoConstraints = false
+        postingDeletePopUpView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            postingEditPopUpView.topAnchor.constraint(equalTo: view.topAnchor),
-            postingEditPopUpView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            postingEditPopUpView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            postingEditPopUpView.heightAnchor.constraint(equalTo: view.heightAnchor)
+//            postingEditPopUpView.topAnchor.constraint(equalTo: view.topAnchor),
+//            postingEditPopUpView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            postingEditPopUpView.widthAnchor.constraint(equalTo: view.widthAnchor),
+//            postingEditPopUpView.heightAnchor.constraint(equalTo: view.heightAnchor),
+//
+            postingDeletePopUpView.topAnchor.constraint(equalTo: view.topAnchor),
+            postingDeletePopUpView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            postingDeletePopUpView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            postingDeletePopUpView.heightAnchor.constraint(equalTo: view.heightAnchor)
         ])
     }
     
     @objc func postingDeleteButtonTapped() {
-        // TODO: addTarget View 간 연결 error 해결하기
-        // view.willRemoveSubview(postingEditPopUpView)
+        // 기존의 view 지우고 delete view 띄우기
+        // 정보 전달하기
+        
     }
 }
