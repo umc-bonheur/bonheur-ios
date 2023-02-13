31db37fc503e87ebec1a337d1e157af389a94451
SohyeonKim-dev <happythgus89@gmail.com>
2023-02-13 14:14:42 +0900

    [FEAT] DeleteCompletionView 구현, PopUpView UI 에러 해결

--- bonheur/Sources/ViewControllers/HomeView/DeleteView/PostingDeletePopUpView.swift
+++ bonheur/Sources/ViewControllers/HomeView/DeleteView/PostingDeletePopUpView.swift
@@ -18,31 +18,50 @@ class PostingDeletePopUpView: UIView {
         view.heightAnchor.constraint(equalToConstant: 213).isActive = true
         return view
     }()
-    
-    let deleteGuidingText: UITextView = {
-        let deleteGuidingText = UITextView()
-        deleteGuidingText.text = "행복 기록을 삭제하겠습니까?"
-        deleteGuidingText.textColor = UIColor(red: 0.149, green: 0.15, blue: 0.149, alpha: 1)
-        deleteGuidingText.font = UIFont(name: "SFPro-Medium", size: 14)
-        deleteGuidingText.textAlignment = .center
-        return deleteGuidingText
+
+     let deleteGuidingLabel: UILabel = {
+         let label = UILabel()
+         label.text = "행복 기록을 삭제하겠습니까?"
+         label.textAlignment = .center
+         label.font = UIFont(name: "SFPro-Bold", size: 14)
+         label.textColor = UIColor(red: 0.149, green: 0.15, blue: 0.149, alpha: 1)
+         return label
+     }()
+
+    let warningGuidingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "삭제된 행복 기록은 되돌릴 수 없습니다"
+        label.textAlignment = .center
+        label.font = UIFont(name: "SFPro-Regular", size: 12)
+        label.textColor = UIColor(red: 0.544, green: 0.55, blue: 0.545, alpha: 1)
+        return label
     }()
     
-    let warningGuidingText: UITextView = {
-        let warningGuidingText = UITextView()
-        warningGuidingText.text = "삭제된 행복 기록은 되돌릴 수 없습니다"
-        warningGuidingText.textColor = UIColor(red: 0.544, green: 0.55, blue: 0.545, alpha: 1)
-        warningGuidingText.font = UIFont(name: "SFPro-Regular", size: 12)
-        warningGuidingText.textAlignment = .center
-        return warningGuidingText
+    let popUpDeleteButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("삭제하기", for: .normal)
+        button.titleLabel?.font = UIFont(name: "SFPro-Medium", size: 14)
+        button.setTitleColor(UIColor(red: 1, green: 0.341, blue: 0.196, alpha: 1), for: .normal)
+        button.backgroundColor = .clear
+        return button
     }()
     
-    let dividerLine = dividerUIView(height: 0.5, color: UIColor(red: 0.842, green: 0.85, blue: 0.842, alpha: 1))
+    let popUpCancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("취소하기", for: .normal)
+        button.titleLabel?.font = UIFont(name: "SFPro-Regular", size: 14)
+        button.setTitleColor(UIColor(red: 0.149, green: 0.15, blue: 0.149, alpha: 1), for: .normal)
+        button.backgroundColor = .clear
+        return button
+    }()
+    
+    let firstDividerLine = dividerUIView(height: 0.5, color: UIColor(red: 0.842, green: 0.85, blue: 0.842, alpha: 1))
+    let secondDividerLine = dividerUIView(height: 0.5, color: UIColor(red: 0.842, green: 0.85, blue: 0.842, alpha: 1))
 
     init() {
         super.init(frame: .zero)
         
-        [backgroundView, deleteGuidingText, warningGuidingText, dividerLine].forEach {
+        [backgroundView, deleteGuidingLabel, warningGuidingLabel, firstDividerLine, secondDividerLine, popUpDeleteButton, popUpCancelButton].forEach {
             addSubview($0)
         }
         
@@ -51,25 +70,40 @@ class PostingDeletePopUpView: UIView {
     
     private func setConstraints() {
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
-        deleteGuidingText.translatesAutoresizingMaskIntoConstraints = false
-        warningGuidingText.translatesAutoresizingMaskIntoConstraints = false
+        deleteGuidingLabel.translatesAutoresizingMaskIntoConstraints = false
+        warningGuidingLabel.translatesAutoresizingMaskIntoConstraints = false
+        popUpDeleteButton.translatesAutoresizingMaskIntoConstraints = false
+        popUpCancelButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             backgroundView.topAnchor.constraint(equalTo: topAnchor, constant: 328),
             backgroundView.centerXAnchor.constraint(equalTo: centerXAnchor),
             
-            // TODO: Text 표시 X error
-            deleteGuidingText.topAnchor.constraint(equalTo: topAnchor, constant: 354),
-            deleteGuidingText.centerXAnchor.constraint(equalTo: centerXAnchor),
-            deleteGuidingText.widthAnchor.constraint(equalTo: backgroundView.widthAnchor),
+            deleteGuidingLabel.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 26),
+            deleteGuidingLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            deleteGuidingLabel.widthAnchor.constraint(equalTo: backgroundView.widthAnchor),
             
-            warningGuidingText.topAnchor.constraint(equalTo: topAnchor, constant: 391),
-            warningGuidingText.centerXAnchor.constraint(equalTo: centerXAnchor),
-            warningGuidingText.widthAnchor.constraint(equalTo: backgroundView.widthAnchor),
+            warningGuidingLabel.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 63),
+            warningGuidingLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            warningGuidingLabel.widthAnchor.constraint(equalTo: backgroundView.widthAnchor),
             
-            dividerLine.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 110),
-            dividerLine.centerXAnchor.constraint(equalTo: centerXAnchor),
-            dividerLine.widthAnchor.constraint(equalTo: backgroundView.widthAnchor)
+            firstDividerLine.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 110),
+            firstDividerLine.centerXAnchor.constraint(equalTo: centerXAnchor),
+            firstDividerLine.widthAnchor.constraint(equalTo: backgroundView.widthAnchor),
+            
+            secondDividerLine.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 159),
+            secondDividerLine.centerXAnchor.constraint(equalTo: centerXAnchor),
+            secondDividerLine.widthAnchor.constraint(equalTo: backgroundView.widthAnchor),
+            
+            popUpDeleteButton.topAnchor.constraint(equalTo: firstDividerLine.bottomAnchor),
+            popUpDeleteButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            popUpDeleteButton.widthAnchor.constraint(equalTo: backgroundView.widthAnchor),
+            popUpDeleteButton.heightAnchor.constraint(equalToConstant: 49),
+            
+            popUpCancelButton.topAnchor.constraint(equalTo: secondDividerLine.bottomAnchor),
+            popUpCancelButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            popUpCancelButton.widthAnchor.constraint(equalTo: backgroundView.widthAnchor),
+            popUpCancelButton.heightAnchor.constraint(equalToConstant: 54)
         ])
     }
     
--- bonheur/Sources/ViewControllers/HomeView/DeleteView/PostingEditPopUpViewController.swift
+++ bonheur/Sources/ViewControllers/HomeView/DeleteView/PostingEditPopUpViewController.swift
@@ -8,17 +8,21 @@
 import UIKit
 
 class PostingEditPopUpViewController: UIViewController {
-//    private let postingEditPopUpView = PostingEditPopUpView()
+    private let postingEditPopUpView = PostingEditPopUpView()
     private let postingDeletePopUpView = PostingDeletePopUpView()
+    private let deleteCompletionView = DeleteCompletionView()
+    // observable Bool로 화면 변환 처리
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clear
         view.layer.backgroundColor = UIColor(red: 0.149, green: 0.15, blue: 0.149, alpha: 0.4).cgColor
 
-//        postingEditPopUpView.postingDeleteButton.addTarget(self, action: #selector(self.postingDeleteButtonTapped), for: .touchUpInside)
-//
-        [postingDeletePopUpView].forEach {
+        postingEditPopUpView.postingDeleteButton.addTarget(self, action: #selector(self.postingDeleteButtonTapped), for: .touchUpInside)
+        postingDeletePopUpView.popUpDeleteButton.addTarget(self, action: #selector(self.popUpDeleteButtonTapped), for: .touchUpInside)
+        postingDeletePopUpView.popUpCancelButton.addTarget(self, action: #selector(self.popUpCancelButtonTapped), for: .touchUpInside)
+
+        [postingEditPopUpView, postingDeletePopUpView, deleteCompletionView].forEach {
             view.addSubview($0)
         }
         
@@ -26,25 +30,39 @@ class PostingEditPopUpViewController: UIViewController {
     }
     
     private func setConstraints() {
-//        postingEditPopUpView.translatesAutoresizingMaskIntoConstraints = false
+        postingEditPopUpView.translatesAutoresizingMaskIntoConstraints = false
         postingDeletePopUpView.translatesAutoresizingMaskIntoConstraints = false
+        deleteCompletionView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-//            postingEditPopUpView.topAnchor.constraint(equalTo: view.topAnchor),
-//            postingEditPopUpView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            postingEditPopUpView.widthAnchor.constraint(equalTo: view.widthAnchor),
-//            postingEditPopUpView.heightAnchor.constraint(equalTo: view.heightAnchor),
-//
+            // TODO: 화면 변환 로직 구현하기
+            postingEditPopUpView.topAnchor.constraint(equalTo: view.topAnchor),
+            postingEditPopUpView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            postingEditPopUpView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            postingEditPopUpView.heightAnchor.constraint(equalTo: view.heightAnchor),
+
             postingDeletePopUpView.topAnchor.constraint(equalTo: view.topAnchor),
             postingDeletePopUpView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             postingDeletePopUpView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            postingDeletePopUpView.heightAnchor.constraint(equalTo: view.heightAnchor)
+            postingDeletePopUpView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            
+            deleteCompletionView.topAnchor.constraint(equalTo: view.topAnchor),
+            deleteCompletionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            deleteCompletionView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            deleteCompletionView.heightAnchor.constraint(equalTo: view.heightAnchor)
         ])
     }
     
     @objc func postingDeleteButtonTapped() {
-        // 기존의 view 지우고 delete view 띄우기
-        // 정보 전달하기
+        // cell data 전달하기
+        
+    }
+    
+    @objc func popUpDeleteButtonTapped() {
+        
+    }
+    
+    @objc func popUpCancelButtonTapped() {
         
     }
 }
