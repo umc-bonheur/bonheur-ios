bf24c8ed86e09dfaed565f48965619974ae38592
SohyeonKim-dev <happythgus89@gmail.com>
2023-02-15 23:27:10 +0900

    [FEAT] getTotalBoardRequest

--- bonheur/Sources/NetworkServices/HomeView/Board/BoardAPI.swift
+++ bonheur/Sources/NetworkServices/HomeView/Board/BoardAPI.swift
@@ -10,19 +10,18 @@ import Moya
 
 public class BoardAPI {
     static let shared = BoardAPI()
-    // var boardProvider = MoyaProvider<BoardService>(plugins: [MoyaLoggerPlugin()])
+    var boardProvider = MoyaProvider<BoardService>(plugins: [MoyaLoggerPlugin()])
     
     public init() { }
-    
-    /*
-    func createTag(createTagRequest: CreateTagRequest, completion: @escaping (NetworkResult<Any>) -> Void) {
-        tagProvider.request(.createTag(createTagRequest: createTagRequest)) { (result) in
+
+    func getTotalBoard(getTotalBoardRequest: GetTotalBoardRequest, completion: @escaping (NetworkResult<Any>) -> Void) {
+        boardProvider.request(.getTotalBoard(getTotalBoardRequest: getTotalBoardRequest)) { (result) in
             switch result {
             case .success(let response):
                 let statusCode = response.statusCode
                 let data = response.data
                 
-                let networkResult = self.judgeCreateTagStatus(by: statusCode, data)
+                let networkResult = self.judgeGetTotalBoardStatus(by: statusCode, data)
                 completion(networkResult)
                 
             case .failure(let error):
@@ -31,24 +30,7 @@ public class BoardAPI {
         }
     }
     
-    func getTag(tagName: String, completion: @escaping (NetworkResult<Any>) -> Void) {
-        tagProvider.request(.getTag(tagName: tagName)) { (result) in
-            switch result {
-            case .success(let response):
-                let statusCode = response.statusCode
-                let data = response.data
-                
-                let networkResult = self.judgeGetTagStatus(by: statusCode, data)
-                completion(networkResult)
-                
-            case .failure(let error):
-                print("error: \(error)")
-            }
-            
-        }
-    }
-    
-    private func judgeCreateTagStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+    private func judgeGetTotalBoardStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
         let decoder = JSONDecoder()
         guard let decodedData = try? decoder.decode(GenericResponse<CreateTagResponse>.self, from: data) else { return .pathError }
         
@@ -63,21 +45,4 @@ public class BoardAPI {
             return .networkFail
         }
     }
-
-    private func judgeGetTagStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
-        let decoder = JSONDecoder()
-        guard let decodedData = try? decoder.decode(GenericResponse<String>.self, from: data) else { return .pathError }
-        
-        switch statusCode {
-        case 200:
-            return .success(decodedData.data ?? "None-data")
-        case 400..<500:
-            return .requestError(decodedData.resultCode, decodedData.message)
-        case 500:
-            return .serverError
-        default:
-            return .networkFail
-        }
-    }
-     */
 }
--- bonheur/Sources/NetworkServices/HomeView/Board/BoardService.swift
+++ bonheur/Sources/NetworkServices/HomeView/Board/BoardService.swift
@@ -9,58 +9,48 @@ import UIKit
 import Moya
 
 enum BoardService {
-
     case getTotalBoard(getTotalBoardRequest: GetTotalBoardRequest)
+    /*
     case createBoard(createBoardRequest: CreateBoardRequest)
     case taggedBoard(taggedBoardRequest: TaggedBoardRequest)
     case getBoardInfo(boardId: Int)
     case deleteBoard(boardId: Int)
-    // case updateBoard() -> TODO: parmas + body로 구현
+    case updateBoard() -> TODO: parmas + body로 구현
+    */
 }
 
-/*
- extension BoardService: TargetType {
- var baseURL: URL {
- return URL(string: Const.URL.baseURL)!
- }
- 
- var path: String {
- switch self {
- case .getTotalBorad:
- return "/boards"
- case .getTag(let tagName):
- return "/tag/\(tagName)"
- }
- }
- 
- var method: Moya.Method {
- switch self {
- case .createTag:
- return .post
- case .getTag:
- return .get
- }
- }
- 
- var task: Moya.Task {
- switch self {
- case .createTag(let createTagRequest):
- return .requestJSONEncodable(createTagRequest)
- 
- case .getTag:
- return .requestPlain
- }
- }
- 
-  MARK: - HTTP Header
- var headers: [String: String]? {
- switch self {
- case .createTag, .getTag:
- let sessionId = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.sessionId)
- let authorizationHeader = ["Content-Type": "application/json", "Authorization": sessionId ?? ""]
- return authorizationHeader
- }
- }
- }
- 
- */
+extension BoardService: TargetType {
+    var baseURL: URL {
+        return URL(string: Const.URL.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getTotalBoard:
+            return "/boards"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getTotalBoard:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getTotalBoard(let getTotalBoardRequest):
+            return .requestJSONEncodable(getTotalBoardRequest)
+        }
+    }
+    
+    var headers: [String: String]? {
+        switch self {
+        case .getTotalBoard:
+            let sessionId = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.sessionId)
+            let authorizationHeader = ["Content-Type": "application/json", "Authorization": sessionId ?? ""]
+            return authorizationHeader
+        }
+    }
+}
